c0ad5b67c50377564ffc85b2bbac1aa7afd072fd
Ovidiu Deac <ovidiu@agile-bit.com>
2016-11-16 11:33:04 +0200

    basic impl

--- prerequisites/impl.fs
+++ prerequisites/impl.fs
@@ -1,10 +1,10 @@
 ﻿module impl
 
 /// Returns the number a * 2
-let twice (a : int) : int = failwith "not implemented"
+let twice (a : int) : int = a * 2
 
 /// Adds the numbers a and b
-let addNumbers (a : int) (b : int) : int = failwith "not implemented"
+let addNumbers (a : int) (b : int) : int = a + b
 
 /// Returns a list containing n times the number x 
 let replicate (x : int) (n : int) : int list = failwith "not implemented"

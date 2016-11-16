71ce5f51f34bd0b2dc9a4e460f9b8d389a6284b8
Ovidiu Deac <ovidiu@agile-bit.com>
2016-11-16 11:37:39 +0200

    implement all

--- prerequisites/impl.fs
+++ prerequisites/impl.fs
@@ -7,8 +7,16 @@ let twice (a : int) : int = a * 2
 let addNumbers (a : int) (b : int) : int = a + b
 
 /// Returns a list containing n times the number x 
-let replicate (x : int) (n : int) : int list = failwith "not implemented"
+let replicate (x : int) (n : int) : int list = List.init n (fun _ -> x)
 
 /// Returns a list containing the first n Fibonacci numbers
 /// https://en.wikipedia.org/wiki/Fibonacci_number
-let fibonacci (n : int) : int list = failwith "not implemented"
+let fibonacci (n : int) : int list = 
+    let rec impl current next =
+        seq {
+            yield current
+            yield! impl next (current + next)
+        }
+    impl 0 1
+    |> Seq.take n
+    |> List.ofSeq
